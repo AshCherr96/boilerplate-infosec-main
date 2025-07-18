2fc570fbaeab5b2f1f0b1d33b5cfe96d9db8b73c
AshCherr96 <ashleycherry16@gmail.com>
2025-07-18 12:29:55 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Useful programmer info secur app started on port ${PORT}`);
+});
+
 
 
 
